ea3dab553fb94fdec259b97a55982fcb93b160e6
Mohammed Abdul Akif <139588023+Akif245@users.noreply.github.com>
2024-05-05 23:44:36 +0530

    ListBox in tkinter

---
+++ list_in_Tk.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import messagebox as msg
+root=Tk()
+root.geometry("444x555")
+root.title("List in Tkinter")
+lst=Listbox(root)
+lst.pack()
+
+
+
+
+
+
+
+
+root.mainloop()
