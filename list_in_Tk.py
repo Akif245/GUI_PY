b52a2c566bbd6709d1980369c335116cc992b3f8
Mohammed Abdul Akif <139588023+Akif245@users.noreply.github.com>
2024-05-07 23:40:28 +0530

    Adding list in kinter

--- list_in_Tk.py
+++ list_in_Tk.py
@@ -3,8 +3,15 @@ from tkinter import messagebox as msg
 root=Tk()
 root.geometry("444x555")
 root.title("List in Tkinter")
+def add ():
+    global i
+    lst.insert(ACTIVE,f"{i}")
+    i+=1
+i=0
 lst=Listbox(root)
+lst.insert(END,"First item of this list ")
 lst.pack()
+Button(root,text="Add item",command=add).pack()
 
 
 
