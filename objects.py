9f5a39fb75091c7d8a7f95f5e2a02668fb4c63ef
Mohammed Abdul Akif <139588023+Akif245@users.noreply.github.com>
2024-05-17 00:07:15 +0530

    Add files via upload
    Object and Class

---
+++ objects.py
@@ -0,0 +1,19 @@
+from tkinter import *
+# import time as tm
+
+class GUI(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("444x555")
+if __name__=='__main__':
+    window=GUI()
+    window.mainloop()
+
+
+
+
+
+
+
+
+
