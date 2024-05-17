1b824336e4b7037cecbf5004133f090989bc8402
Mohammed Abdul Akif <139588023+Akif245@users.noreply.github.com>
2024-05-18 00:17:02 +0530

    Add files via upload
    image

---
+++ image.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root=Tk()
+root.geometry("444x555")
+root.title("image")
+root.configure(background="blue")
+# root.wm_iconbitmap("2.ico")
+width=root.winfo_screenwidth()
+height=root.winfo_screenheight()
+print(f"{width}x{height}")
+root.mainloop()
+
+
